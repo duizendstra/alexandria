dcb4d94cdb3521fdcb5920f17028464f2c76b903
Jasper Duizendstra <jasper@duizendstra.com>
2026-07-12 14:47:38 +0200

    fix(contracts): satisfy package doc comment style for revive linter

--- go/contracts/doc.go
+++ go/contracts/doc.go
@@ -1,5 +1,5 @@
 // Copyright 2026 Duizendstra. All rights reserved.
-//
+
 // Package contracts provides unified, compiled Protocol Buffer messages
 // and ConnectRPC services representing the formal Ubiquitous Language
 // of the Alexandria platform.
